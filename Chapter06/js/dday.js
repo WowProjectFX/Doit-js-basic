39ce53ad609dae894d6db0a300448889e0756bf8
WowProjectFX <wowprojectfx@gmail.com>
2019-12-11 16:31:18 +0900

    기념일 계산 프로그램 만들기 - 함수 사용하기

--- Chapter06/js/dday.js
+++ Chapter06/js/dday.js
@@ -4,17 +4,19 @@ let toNow = now.getTime();
 let toFirst = firstDay.getTime();
 let passedTime = toNow - toFirst;
 let passedDay = Math.round(passedTime/(1000*60*60*24));
-let future = toFirst + 100 * (24 * 60 * 60 * 1000);
-let someDay = new Date(future);
+document.querySelector('#accent').innerText = passedDay + "일";
 
-function SomeDay () {
-    this.set
+function calcDate (days) {
+    let future = toFirst + days * (24 * 60 * 60 * 1000);
+    let someDay = new Date(future);
+    let year = someDay.getFullYear();
+    let month = someDay.getMonth() + 1;
+    let date = someDay.getDate();
+    document.querySelector('#date' + days).innerText = year + "년 " + month + "월 " + date + "일";
 }
 
-let year = someDay.getFullYear();
-let month = someDay.getMonth();
-let date = someDay.getDate();
 
-document.querySelector('#accent').innerText = passedDay + "일";
-document.querySelector('#date100').innerText = year + "년 " + month + "월 " + date + "일";
-
+calcDate(100);
+calcDate(200);
+calcDate(365);
+calcDate(1000);
